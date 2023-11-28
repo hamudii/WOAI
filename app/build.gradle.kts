ea51dbcbbf97d69bb41d2d08cc5246d50c8e5bb2
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-11-28 18:56:48 +0700

    Add blur effect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -39,6 +40,7 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
 dependencies {
@@ -50,6 +52,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("io.alterac.blurkit:blurkit:1.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    kapt ("com.github.bumptech.glide:compiler:4.12.0")
 
+    implementation("com.github.Dimezis:BlurView:version-2.0.3")
 }
--- app/src/main/java/com/bangkit/woai/views/strated/GetStartedAct.kt
+++ app/src/main/java/com/bangkit/woai/views/strated/GetStartedAct.kt
@@ -1,13 +1,19 @@
 package com.bangkit.woai.views.strated
 
-
-import android.graphics.drawable.BitmapDrawable
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.view.animation.Animation
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.bangkit.woai.R
 import com.bangkit.woai.databinding.ActivityGetStartedBinding
-import com.bangkit.woai.views.BlurUtils
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.bangkit.woai.R
+import com.bangkit.woai.views.main.MainActivity
+import eightbitlab.com.blurview.RenderScriptBlur
 
 class GetStartedAct : AppCompatActivity() {
     private lateinit var binding: ActivityGetStartedBinding
@@ -17,5 +23,15 @@ class GetStartedAct : AppCompatActivity() {
         binding = ActivityGetStartedBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        val radius = 20f
+        binding.blurView.setupWith(binding.container)
+            .setBlurRadius(radius)
+
+        //intent to main act
+        binding.btnStarted.setOnClickListener {
+            val mainAct = Intent(this, MainActivity::class.java)
+            startActivity(mainAct)
+        }
     }
 }
